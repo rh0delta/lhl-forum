0f68b322ea6ea7176d99917d998d74ab6cf36381
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-07-25 18:28:11 +0000

    adding new post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
+  get 'posts',     to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
-
+  post 'posts',    to: 'posts#create'
 
 end
